c0d7bbc5dc49dfe79a892de3be07403e82d339a0
Subhasis Ray <ray.subhasis@gmail.com>
2025-05-05 13:10:37 +0530

    Added publications bibtex file and utility function for it
    - Added bibliography file in bibtex format
    - Added APA-CV stylesheet from zotero
    - Added utility function to process the bibliography contents

---
+++ README.md
@@ -0,0 +1,13 @@
+# This is the website for MOOSE
+Setting up the environment:
+
+Packages required for citation handling:
+```
+npm install citeproc bibtex-parse
+```
+
+Publications using moose are listed in
+
+```
+public/publications.bib
+```
--- src/utils.ts
+++ src/utils.ts
@@ -8,3 +8,35 @@ function formatDate(date: Date): string {
 }
 
 export { formatDate };
+
+import { parse } from 'bibtex-parse';
+import { CiteProc } from 'citeproc';
+import fs from 'fs';
+
+// 1. Load your BibTeX file
+const bibtex = fs.readFileSync('./public/assets/publications.bib', 'utf-8');
+
+// 2. Parse BibTeX to CSL-JSON
+export function getCitations() {
+  const entries = parse(bibtex);
+  return Object.values(entries);
+}
+
+// 3. Initialize Citeproc
+export function formatCitations(style = 'apa-cv') {
+  const citations = getCitations();
+  const styleFile = fs.readFileSync(`./public/csl/${style}.csl`, 'utf-8');
+  
+  const citeproc = new CiteProc({
+    retrieveLocale: () => '',
+    retrieveItem: (id) => citations.find(item => item.id === id)
+  });
+
+  citeproc.updateItems(citations.map(item => item.id));
+  citeproc.setStyle(styleFile);
+
+  return citations.map(item => ({
+    id: item.id,
+    citation: citeproc.makeBibliography()[1].join('')
+  }));
+}
