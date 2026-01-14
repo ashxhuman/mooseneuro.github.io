89dd1c1054732730f392510e3f5e94f3047afc5b
ashxhuman <ashish@ncbs.res.in>
2026-01-14 16:06:52 +0530

    update for new release v4.1.4 and jardesigner button

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,9 @@
 import workshops from "./workshops/workshops.config";
 import team from "./team/team.config";
+import news from "./news/news.config";
 
 export const collections = {
   workshops: workshops,
   team: team,
+  news: news,
 };
