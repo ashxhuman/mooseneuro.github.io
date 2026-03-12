5b90b5dcab8742d7a4668d3881291a0adfdd8c8c
ashxhuman <ashish@ncbs.res.in>
2026-03-12 15:21:05 +0530

    Fix Funders Logo

---
+++ src/assets/PastFunders/pastFunders.ts
@@ -0,0 +1,7 @@
+// funders.ts
+import DBTLogo from "../PastFunders/DBT-logo.png";
+import NIHLogo from "../PastFunders/NIH_2013_logo_vertical.svg"
+export const pastFunders = [
+  { name: "NIH", src: NIHLogo, },
+  { name: "DBT", src: DBTLogo },
+];
